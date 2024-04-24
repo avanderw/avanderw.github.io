eedd806f403c1bc7a51c8d0ed4fc4d031cd05d8f
Andrew van der Westhuizen <avanderw@gmail.com>
2024-04-24 15:18:36 +0200

    Update tracking URL

--- src/routes/Tracking.svelte
+++ src/routes/Tracking.svelte
@@ -3,7 +3,7 @@
 <p>
 	I track statistics on my site, but I do not track any personal information. I use a
 	self-hosted instance and I make the data available so that you can <a
-		href="https://avanderw.tplinkdns.com:31024/avanderw.co.za/"
+		href="https://tracking.avanderw.co.za/avanderw.co.za/"
 		title="Plausible analytics">see the analytics</a
 	> for yourself.
 </p>
