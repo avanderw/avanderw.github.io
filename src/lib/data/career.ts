051657db93cec01ca982304deaf9ba68e72778a5
Andrew van der Westhuizen <avanderw@gmail.com>
2025-07-17 00:28:12 +0200

    fix: update career entries for consistency in role titles and company names

--- src/lib/data/career.ts
+++ src/lib/data/career.ts
@@ -3,30 +3,44 @@ import type { CareerEntry } from '../types.js';
 export const careerEntries: CareerEntry[] = [
 	{
 		period: '2014 -',
-		roles: ['Product Head', 'Development Manager', 'Scrum Master', 'Senior Developer'],
+		roles: ['Product Head', 'Development Manager', 'Scrum Master', 'Senior Software Developer'],
 		company: 'Capitec Bank',
 		companyUrl: 'https://www.capitecbank.co.za/',
 		location: 'Cape Town'
 	},
 	{
 		period: '2010 - 2014',
-		roles: ['Senior Developer'],
+		roles: ['Senior Software Developer'],
 		company: 'University of Stellenbosch',
 		companyUrl: 'https://www.sun.ac.za/',
 		location: 'Cape Town'
 	},
 	{
+		period: '2011 - 2012',
+		roles: [ 'Master of Business Administration (MBA)'],
+		company: 'University of Cape Town',
+		companyUrl: 'https://www.gsb.uct.ac.za/',
+		location: 'Cape Town'
+	},
+	{
 		period: '2007 - 2010',
-		roles: ['Developer'],
-		company: 'eSight',
+		roles: ['Software Developer'],
+		company: 'eSight Consulting',
 		companyUrl: '?#',
 		location: 'Cape Town'
 	},
 	{
 		period: '2003 - 2004',
-		roles: ['Tutor'],
+		roles: ['Math Tutor'],
 		company: 'Master Maths',
 		companyUrl: 'https://www.mastermaths.co.za/',
+		location: 'Cape Town'
+	},
+	{
+		period: '2002 - 2006',
+		roles: ['Honours in Computer Science'],
+		company: 'University of Cape Town',
+		companyUrl: 'https://sit.uct.ac.za/honours-computer-science',
 		location: 'Cape Town'
 	}
 ];
